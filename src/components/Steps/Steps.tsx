bc6a4a138b24323bfb372bc2ce2cf53363459773
Ghalem Salim <salimghalem40@gmail.com>
2024-12-03 18:50:04 +0100

    steps done

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -1,6 +1,23 @@
 import './Steps.css';
 const Steps = () => {
-  return <section className="steps">Steps</section>;
+  return (
+    <section className="steps">
+      <h2>Tokenisation</h2>
+      <div className="container">
+        <h4>phrase=['une','phrase','de','test']</h4>
+      </div>
+      <h2>Collocation de mots</h2>
+      <div className="container">
+        <h4>phrase=['une phrase','de','test']</h4>
+      </div>
+      <h2>Filtrage des stop-words</h2>
+      <div className="container">
+        <h4>phrase=['une phrase','test']</h4>
+      </div>
+      <h2>Ponderation</h2>
+      <h2>Fichier Inverse</h2>
+    </section>
+  );
 };
 
 export default Steps;
